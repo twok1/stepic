6ff5a1b223f35f67b0cefdc6d4a90aa0730af73e
twok1 <twokey25@gmail.com>
2022-09-06 16:41:02 +0300

    функции высшего порядка, встроенные функции map, filter, reduce анонимные функции встроенные функции all, any, enumerate

--- stepic.py
+++ stepic.py
@@ -2324,9 +2324,9 @@ def func_fractions():
 def func_fractions_2():
     s = list(map(Fraction,
                  '0.78 4.3 9.6 3.88 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 '
-                 '1.56 6.67 ' \ 
+                 '1.56 6.67 ' \
                  '1.46 5.26 4.83 7.13 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 '
-                 '5.78 ' \ 
+                 '5.78 ' \
                  '5.37 0.03 9.60 8.86 2.73 5.83 6.50 0.123 0.00021'.split()))
     print(min(s) + max(s))
 
@@ -2519,30 +2519,30 @@ def non_decreasing_mean_2(point):
 n = 2
 
 
-def map(function, items):
-    result = []
-    for item in items:
-        result.append(function(item, n))
-    return result
+# def map(function, items):
+#     result = []
+#     for item in items:
+#         result.append(function(item, n))
+#     return result
 
 
 # numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828,
 #            1.41546]
 # print(*map(round, numbers))
 
-def map(function, items):
-    result = []
-    for item in items:
-        result.append(function(item))
-    return result
+# def map(function, items):
+#     result = []
+#     for item in items:
+#         result.append(function(item))
+#     return result
 
 
-def filter(function, items):
-    result = []
-    for item in items:
-        if function(item):
-            result.append(item)
-    return result
+# def filter(function, items):
+#     result = []
+#     for item in items:
+#         if function(item):
+#             result.append(item)
+#     return result
 
 
 def lenny(num):
@@ -2553,12 +2553,290 @@ def cubes(num):
     return num ** 3
 
 
-numbers = [1014, 1321, 675, 1215, 56, 1386, 1385, 431, 1058, 486, 1434, 696, 1016, 1084, 424, 1189, 475, 95, 1434, 1462,
-           815, 776, 657, 1225, 912, 537, 1478, 1176, 544, 488, 668, 944, 207, 266, 1309, 1027, 257, 1374, 1289, 1155,
-           230, 866, 708, 144, 1434, 1163, 345, 394, 560, 338, 232, 182, 1438, 1127, 928, 1309, 98, 530, 1013, 898, 669,
-           105, 130, 1363, 947, 72, 1278, 166, 904, 349, 831, 1207, 1496, 370, 725, 926, 175, 959, 1282, 336, 1268, 351,
-           1439, 186, 273, 1008, 231, 138, 142, 433, 456, 1268, 1018, 1274, 387, 120, 340, 963, 832, 1127]
+# numbers = [1014, 1321, 675, 1215, 56, 1386, 1385, 431, 1058, 486, 1434, 696, 1016, 1084, 424, 1189, 475, 95, 1434, 1462,
+#            815, 776, 657, 1225, 912, 537, 1478, 1176, 544, 488, 668, 944, 207, 266, 1309, 1027, 257, 1374, 1289, 1155,
+#            230, 866, 708, 144, 1434, 1163, 345, 394, 560, 338, 232, 182, 1438, 1127, 928, 1309, 98, 530, 1013, 898, 669,
+#            105, 130, 1363, 947, 72, 1278, 166, 904, 349, 831, 1207, 1496, 370, 725, 926, 175, 959, 1282, 336, 1268, 351,
+#            1439, 186, 273, 1008, 231, 138, 142, 433, 456, 1268, 1018, 1274, 387, 120, 340, 963, 832, 1127]
+#
+# numbers = filter(lenny, numbers)
+# numbers = map(cubes, numbers)
+# pass
+# def reduce(operation, items, initial_value):
+#     acc = initial_value
+#     for item in items:
+#         acc = operation(acc, item)
+#     return acc
 
-numbers = filter(lenny, numbers)
-numbers = map(cubes, numbers)
-pass
+
+# def sqr(acc, item):
+#     return acc + item**2
+#
+# numbers = [97, 42, 9, 32, 3, 45, 31, 77, -1, 11, -2, 75, 5, 51, 34, 28, 46, 1, -8, 84, 16, 51, 90, 56, 65, 90, 23, 35, 11, -10, 70, 90, 90, 12, 96, 58, -8, -4, 91, 76, 94, 60, 72, 43, 4, -6, -5, 51, 58, 60, 30, 38, 67, 62, 36, 72, 34, 82, 62, -1, 60, 82, 87, 81, -7, 57, 26, 36, 17, 43, 80, 40, 75, 94, 91, 64, 38, 72, 29, 84, 38, 35, 7, 54, 31, 95, 78, 27, 82, 1, 64, 94, 31, 29, -8, 98, 24, 61, 7, 73]
+# print(reduce(sqr, numbers, 0))
+
+def map_2(function, items):
+    result = []
+    for item in items:
+        result.append(function(item))
+    return result
+
+
+def sqr(item):
+    return item ** 2
+
+
+def filter_2(function, items):
+    result = []
+    for item in items:
+        if function(item):
+            result.append(item)
+    return result
+
+
+# def sorting(item):
+#     if len(str(abs(item))) == 2 and item % 7 == 0:
+#         return item
+
+
+# numbers = [77, 293, 28, 242, 213, 285, 71, 286, 144, 276, 61, 298, 280, 214, 156, 227, 228, 51, -4, 202, 58, 99, 270, 219, 94, 253, 53, 235, 9, 158, 49, 183, 166, 205, 183, 266, 180, 6, 279, 200, 208, 231, 178, 201, 260, -35, 152, 115, 79, 284, 181, 92, 286, 98, 271, 259, 258, 196, -8, 43, 2, 128, 143, 43, 297, 229, 60, 254, -9, 5, 187, 220, -8, 111, 285, 5, 263, 187, 192, -9, 268, -9, 23, 71, 135, 7, -161, 65, 135, 29, 148, 242, 33, 35, 211, 5, 161, 46, 159, 23, 169, 23, 172, 184, -7, 228, 129, 274, 73, 197, 272, 54, 278, 26, 280, 13, 171, 2, 79, -2, 183, 10, 236, 276, 4, 29, -10, 41, 269, 94, 279, 129, 39, 92, -63, 263, 219, 57, 18, 236, 291, 234, 10, 250, 0, 64, 172, 216, 30, 15, 229, 205, 123, -105]
+# # numbers = [int(i) for i in numbers if len(str(abs(i))) == 2 and i % 7 == 0]
+# numbers = filter_2(sorting, numbers)
+# print(sum(map_2(sqr, numbers)))
+
+from functools import reduce
+
+
+def less_15_7_1():
+    floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+    words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+    numbers = [4, 6, 9, 23, 5]
+
+    # Исправьте этот код
+    map_result = list(map(lambda num: round(num ** 2, 1), floats))
+    filter_result = list(filter(lambda name: len(name) > 4 and name == name[::-1], words))
+    reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
+
+    print(map_result)
+    print(filter_result)
+    print(reduce_result)
+
+
+# less_15_7_1()
+def less_15_7_2():
+    data = [['Tokyo', 35676000, 'primary'],
+            ['New York', 19354922, 'nan'],
+            ['Mexico City', 19028000, 'primary'],
+            ['Mumbai', 18978000, 'admin'],
+            ['Sao Paulo', 18845000, 'admin'],
+            ['Delhi', 15926000, 'admin'],
+            ['Shanghai', 14987000, 'admin'],
+            ['Kolkata', 14787000, 'admin'],
+            ['Los Angeles', 12815475, 'nan'],
+            ['Dhaka', 12797394, 'primary'],
+            ['Buenos Aires', 12795000, 'primary'],
+            ['Karachi', 12130000, 'admin'],
+            ['Cairo', 11893000, 'primary'],
+            ['Rio de Janeiro', 11748000, 'admin'],
+            ['Osaka', 11294000, 'admin'],
+            ['Beijing', 11106000, 'primary'],
+            ['Manila', 11100000, 'primary'],
+            ['Moscow', 10452000, 'primary'],
+            ['Istanbul', 10061000, 'admin'],
+            ['Paris', 9904000, 'primary']]
+    data = filter(lambda x: x[1] > 10000000 and x[2] == 'primary', data)
+    data = sorted(data)
+    data = map(lambda x: x[0], data)
+    data = reduce(lambda x, y: x + ', ' + y, data)
+    print('Cities:', data)
+
+
+# less_15_7_2()
+
+def less_15_8_1():
+    func = lambda x: True if (x % 13 == 0 or x % 19 == 0) else False
+    print(func(19))
+    print(func(13))
+    print(func(20))
+    print(func(15))
+    print(func(247))
+
+
+# less_15_8_1()
+
+def less_15_8_2():
+    func = lambda x: True if x.lower()[0] == x.lower()[-1] == 'a' else False
+    print(func('abcd'))
+    print(func('bcda'))
+    print(func('abcda'))
+    print(func('Abcd'))
+    print(func('bcdA'))
+    print(func('abcdA'))
+
+
+# less_15_8_2()
+def less_15_8_3():
+    is_non_negative_num = lambda x: x.replace('.', '', 1).isdigit()
+    print(is_non_negative_num('10.34ab'))
+    print(is_non_negative_num('10.45'))
+    print(is_non_negative_num('-18'))
+    print(is_non_negative_num('-34.67'))
+    print(is_non_negative_num('987'))
+    print(is_non_negative_num('abcd'))
+    print(is_non_negative_num('123.122.12'))
+    print(is_non_negative_num('123.122'))
+
+
+# less_15_8_3()
+def less_15_8_4():
+    is_num = lambda x: '-' not in x[1:] and x.replace('.', '', 1).replace('-', '', 1).isdigit()
+    print(is_num('10.34ab'))
+    print(is_num('10.45'))
+    print(is_num('-18'))
+    print(is_num('-34.67'))
+    print(is_num('987'))
+    print(is_num('abcd'))
+    print(is_num('123.122.12'))
+    print(is_num('-123.122'))
+    print(is_num('--13.2'))
+
+
+# less_15_8_4()
+def less_15_8_5():
+    words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able',
+             'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow',
+             'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday',
+             'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept',
+             'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+    words = filter(lambda x: len(x) == 6, words)
+
+    print(*list(sorted(words)))
+
+# less_15_8_5()
+def less_15_8_6():
+    numbers = [46, 61, 34, 17, 56, 26, 93, 1, 3, 82, 71, 37, 80, 27, 77, 94, 34, 100, 36, 81, 33, 81, 66, 83, 41, 80,
+               80, 93, 40, 34, 32, 16, 5, 16, 40, 93, 36, 65, 8, 19, 8, 75, 66, 21, 72, 32, 41, 59, 35, 64, 49, 78, 83,
+               27, 57, 53, 43, 35, 48, 17, 19, 40, 90, 57, 77, 56, 80, 95, 90, 27, 26, 6, 4, 23, 52, 39, 63, 74, 15, 66,
+               29, 88, 94, 37, 44, 2, 38, 36, 32, 49, 5, 33, 60, 94, 89, 8, 36, 94, 46, 33]
+    # numbers = filter(lambda x: x % 2 == 0 or (x < 47 and x % 2 == 1), numbers)
+    # print(list(numbers))
+    numbers = map(lambda x: x//2 if x % 2 == 0 else x, filter(lambda x: x % 2 == 0 or (x < 47 and x % 2 == 1), numbers))
+    print(*numbers)
+
+
+# less_15_8_6()
+def less_15_8_7():
+    data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'),
+            (1805832, 'West Virginia'), (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'),
+            (10077331, 'Michigan'), (10439388, 'Virginia'), (7705281, 'Washington'), (7151502, 'Arizona'),
+            (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
+    data = sorted(data, key=lambda x: x[1][-1], reverse=True)
+    for k, v in data:
+        print(v, k, sep=': ')
+
+# less_15_8_7()
+def less_15_8_8():
+    data = ['год', 'человек', 'время', 'дело', 'жизнь', 'день', 'рука', 'раз', 'работа', 'слово', 'место', 'лицо',
+            'друг', 'глаз', 'вопрос', 'дом', 'сторона', 'страна', 'мир', 'случай', 'голова', 'ребенок', 'сила', 'конец',
+            'вид', 'система', 'часть', 'город', 'отношение', 'женщина', 'деньги']
+    print(*sorted(sorted(data), key=lambda x: len(x)))
+
+# less_15_8_8()
+def less_15_8_9():
+    mixed_list = ['tuesday', 'abroad', 'abuse', 'beside', 'monday', 'abate', 'accessory', 'absorb', 1384878, 'sunday',
+                  'about', 454805, 'saturday', 'abort', 2121919, 2552839, 977970, 1772933, 1564063, 'abduct', 901271,
+                  2680434, 'bicycle', 'accelerate', 1109147, 942908, 'berry', 433507, 'bias', 'bestow', 1875665,
+                  'besides', 'bewilder', 1586517, 375290, 1503450, 2713047, 'abnormal', 2286106, 242192, 701049,
+                  2866491, 'benevolent', 'bigot', 'abuse', 'abrupt', 343772, 'able', 2135748, 690280, 686008, 'beyond',
+                  2415643, 'aboard', 'bet', 859105, 'accident', 2223166, 894187, 146564, 1251748, 2851543, 1619426,
+                  2263113, 1618068, 'berth', 'abolish', 'beware', 2618492, 1555062, 'access', 'absent', 'abundant',
+                  2950603, 'betray', 'beverage', 'abide', 'abandon', 2284251, 'wednesday', 2709698, 'thursday', 810387,
+                  'friday', 2576799, 2213552, 1599022, 'accept', 'abuse', 'abound', 1352953, 'bid', 1805326, 1499197,
+                  2241159, 605320, 2347441]
+    print(max(mixed_list, key=lambda x: x if type(x) == int else 0))
+
+# less_15_8_9()
+def less_15_8_10():
+    mixed_list = ['beside', 48, 'accelerate', 28, 'beware', 'absorb', 'besides', 'berry', 15, 65, 'abate', 'thursday',
+                  76, 70, 94, 35, 36, 'berth', 41, 'abnormal', 'bicycle', 'bid', 'sunday', 'saturday', 87, 'bigot', 41,
+                  'abort', 13, 60, 'friday', 26, 13, 'accident', 'access', 40, 26, 20, 75, 13, 40, 67, 12, 'abuse', 78,
+                  10, 80, 'accessory', 20, 'bewilder', 'benevolent', 'bet', 64, 38, 65, 51, 95, 'abduct', 37, 98, 99,
+                  14, 'abandon', 'accept', 46, 'abide', 'beyond', 19, 'about', 76, 26, 'abound', 12, 95, 'wednesday',
+                  'abundant', 'abrupt', 'aboard', 50, 89, 'tuesday', 66, 'bestow', 'absent', 76, 46, 'betray', 47,
+                  'able', 11]
+    print(*sorted(mixed_list, key=lambda x: str(x)))
+
+
+# less_15_8_10()
+from operator import sub, add
+
+
+def less_15_8_11():
+    print(*map(sub, (255,255,255), map(int, input().split())))
+
+# less_15_8_11()
+
+
+def less_15_8_12():
+    coeff = list(map(int, input().split()))
+    x = int(input())
+    result = list(map(lambda c, i: c * (x ** i), coeff, range(len(coeff)-1, -1, -1)))
+    # print(result)
+    print(reduce(add, result))
+    # print(evaluate(coeff, x))
+
+
+# less_15_8_12()
+def less_15_9_1():
+    def ignore_command(command):
+        ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+
+        return any(map(lambda x: x in command, ignore))
+
+    print(ignore_command('get ip'))
+    print(ignore_command('select all'))
+    print(ignore_command('delete'))
+    print(ignore_command('trancate'))
+
+# less_15_9_1()
+def less_15_9_2():
+    countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+    capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+    population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+    for cap, countr, pop in zip(capitals, countries, population):
+        print(f'{cap} is the capital of {countr}, population equal {pop} people.')
+
+# less_15_9_2()
+def less_15_9_3():
+    abscissas = list(map(float, input().split()))
+    ordinates = list(map(float, input().split()))
+    applicates = list(map(float, input().split()))
+    print(all([2 ** 2 >= x ** 2 + y ** 2 + z ** 2 for x, y, z in zip(abscissas, ordinates, applicates)]))
+
+# less_15_9_3()
+def less_15_9_4():
+    print(all(map(lambda x: x.isdigit() and 0 <= int(x) <= 255, input().split('.'))))
+
+# less_15_9_4()
+def less_15_9_5():
+    n, m = int(input()), int(input())
+    nums = list(filter(lambda x: True if '0' not in str(x)  else False, range(n, m+1)))
+    nums = list(filter(lambda x: True if all([1 if (x % int(i) == 0) else 0 for i in str(x)]) else False, nums))
+    print(*nums)
+
+
+# less_15_9_5()
+def less_15_9_6():
+    password = input()
+    check = all([len(password) >= 7,
+                 any([i.isupper() for i in password]),
+                 any([i.islower() for i in password]),
+                 any([i.isdigit() for i in password])])
+    if check:
+        print('YES')
+    else:
+        print('NO')
+
+# less_15_9_6()
+def less_15_9_7():
+    pass
